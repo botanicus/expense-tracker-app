b3991eeb01a62f8ad34d8e1e87417e84b34dfe63
James C Russell <james@101ideas.cz>
2014-07-17 17:07:40 +0200

    Working on sign-up.

--- client/app.js
+++ client/app.js
@@ -1,7 +1,19 @@
-var app = angular.module('app', ['ngRoute', 'ui.bootstrap']);
+var app = angular.module('app', ['ngRoute', 'ui.bootstrap', 'services']);
 
 app.config(function ($locationProvider, $routeProvider) {
-  $routeProvider.when('/', {controller: 'HomeController'});
+  $routeProvider.when('/', {
+    controller: 'HomeController',
+    templateUrl: '/templates/home.html'
+  }).
+  when('/sign-up', {
+    controller: 'SignUpController',
+    templateUrl: '/templates/sign-up.html'
+  }).
+  when('/login', {
+    // TODO: How to go about it?
+    controller: 'SignUpController',
+    templateUrl: '/templates/sign-up.html'
+  });
 
   $routeProvider.otherwise({'redirectTo': '/'});
 
@@ -19,4 +31,15 @@ app.controller('MainController', function ($scope) {
 });
 
 app.controller('HomeController', function ($scope) {
+});
+
+app.controller('SignUpController', function ($scope, $location, User) {
+  $scope.user = {};
+
+  $scope.register = function () {
+    var user = new User($scope.user);
+    user.$save();
+    // TODO: Log in.
+    $location.path('/app');
+  };
 });
