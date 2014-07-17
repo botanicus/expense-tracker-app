308e773e4456259a311b61a21ac1761751b3a6fe
James C Russell <james@101ideas.cz>
2014-07-17 09:42:54 +0200

    User model & authentication.

--- server/lib/expenses-tracker/models.rb
+++ server/lib/expenses-tracker/models.rb
@@ -1,1 +1,30 @@
 require 'ohm'
+require 'bcrypt'
+
+module ExpensesTracker
+  class User < Ohm::Model
+    # Public attributes.
+    attribute :username
+    index :username
+    unique :username
+
+    # Private attributes.
+    attribute :salt
+    attribute :encrypted_password
+
+    def self.authenticate(username, password)
+      user = self.find(username: username).first
+      encrypted_password = BCrypt::Engine.hash_secret(password, user.salt)
+      user if user.encrypted_password == encrypted_password
+    rescue Ohm::IndexNotFound
+      # Return nil if user doesn't exist.
+    end
+
+    def password=(password)
+      self.salt = BCrypt::Engine.generate_salt
+      self.encrypted_password = begin
+        BCrypt::Engine.hash_secret(password, self.salt)
+      end
+    end
+  end
+end
---
+++ server/spec/expenses-tracker/models_spec.rb
@@ -0,0 +1,36 @@
+require 'spec_helper'
+require 'expenses-tracker/models'
+
+describe ExpensesTracker::User do
+  it 'has a username attribute' do
+    subject.username = 'botanicus'
+    expect(subject.username).to eq('botanicus')
+  end
+
+  context 'authentication' do
+    before(:each) do
+      subject.username = 'botanicus'
+      subject.password = '123456789'
+      subject.save
+    end
+
+    it 'is possible to log in with valid credentials' do
+      user = described_class.authenticate('botanicus', '123456789')
+      expect(user).not_to be_nil
+    end
+
+    it 'is not possible to log in with invalid credentials' do
+      user = described_class.authenticate('botanicus', '987654321')
+      expect(user).to be_nil
+    end
+
+    it 'is not possible to log in if you are not registered' do
+      user = described_class.authenticate('johndoe', '987654321')
+      expect(user).to be_nil
+    end
+
+    after(:each) do
+      ExpensesTracker::User.redis.call(:flushdb)
+    end
+  end
+end
