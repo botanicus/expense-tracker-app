e1d03fa76cc3fff9b0fa06a7e4bd649c6b4e9304
James C Russell <james@101ideas.cz>
2014-07-23 14:31:45 +0200

    Expense factory.

--- client/services.js
+++ client/services.js
@@ -1,5 +1,9 @@
 var services = angular.module('services', ['ngResource']);
 
-services.factory('User', function ($resource, $location) {
+services.factory('User', function ($resource) {
   return $resource('/api/users/:username');
+});
+
+services.factory('Expense', function ($resource) {
+  return $resource('/api/expenses/:id');
 });
